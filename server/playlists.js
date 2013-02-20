cbdd060c08f0007e2611e07f90b9736cd7f42519
Benjamin Chelli <benjamin@chelli.net>
2013-02-20 23:07:17 +0100

    Add order by names on playlists

--- server/playlists.js
+++ server/playlists.js
@@ -6,5 +6,5 @@ Meteor.publish('playlists', function(userId){
   if(user && user.services && user.services.facebook && user.services.facebook.id){
     query.$or.push({canAccess : user.services.facebook.id});
   }
-  return playlists.find(query);
+  return playlists.find(query, {sort:[['name', 'asc']]});
 });
