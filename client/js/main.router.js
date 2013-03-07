1153064fb8bfe9ecd496af8a31a823d7e7b50a30
Benjamin Chelli <benjamin@chelli.net>
2013-03-07 04:36:00 +0100

    Update Google Analytics to be inited before the backbone routers

--- client/js/main.router.js
+++ client/js/main.router.js
@@ -1,6 +1,15 @@
 
 
 Meteor.startup(function () {
+  // INIT GOOGLE ANALYTICS
+  window._gaq = window._gaq || []
+  _gaq.push(['_setAccount', 'UA-39093550-1'])
+ 
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+  // SET TRACK PAGE VIEW
   function trackPageview(){
     var url = Backbone.history.getFragment();
     window._gaq = window._gaq || [];
@@ -11,5 +20,6 @@ Meteor.startup(function () {
   playlistsRouter.bind('all', trackPageview);
   videosRouter.bind('all', trackPageview);
 
+  // START BACKBONE HISTORY
   Backbone.history.start({pushState: true, root: "/"});
 });
