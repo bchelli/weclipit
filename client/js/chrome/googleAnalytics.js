0fe692171ee537f38c1ebf1ba45b14bc115b75b8
Benjamin Chelli <benjamin@chelli.net>
2013-03-07 04:23:01 +0100

    Add track page view on google analytics

--- client/js/chrome/googleAnalytics.js
+++ client/js/chrome/googleAnalytics.js
@@ -2,7 +2,6 @@
 Template.googleAnalyticsTemplate.rendered = function(){
   window._gaq = window._gaq || []
   _gaq.push(['_setAccount', 'UA-39093550-1'])
-  _gaq.push(['_trackPageview'])
  
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
--- client/js/main.router.js
+++ client/js/main.router.js
@@ -1,5 +1,15 @@
 
 
 Meteor.startup(function () {
+  function trackPageview(){
+    var url = Backbone.history.getFragment();
+    window._gaq = window._gaq || [];
+    window._gaq.push(['_trackPageview', url]);
+  }
+
+  homeRouter.bind('all', trackPageview);
+  playlistsRouter.bind('all', trackPageview);
+  videosRouter.bind('all', trackPageview);
+
   Backbone.history.start({pushState: true, root: "/"});
 });
