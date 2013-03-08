3c47ecd3e34018d8ef8096a89da998f45d70e069
Benjamin Chelli <benjamin@chelli.net>
2013-03-09 00:57:31 +0100

    FIX Twitter Card filter User-Agent to match Twitterbot

--- server/libs/twitter.js
+++ server/libs/twitter.js
@@ -19,9 +19,7 @@ if(Meteor.isServer) {
     __meteor_bootstrap__.app.use(function(req, res, next) {
 
       // IS ACTUALLY TWITTER
-      console.error(req.headers['user-agent']);
-      //if (req.headers['user-agent'].toLowerCase().indexOf('twitterbot') !== -1) {
-      if (req.headers['user-agent'] === 'Twitterbot') {
+      if (req.headers['user-agent'].indexOf('Twitterbot') !== -1) {
 
         res.writeHead(200, {'Content-Type': 'text/html'});
 
