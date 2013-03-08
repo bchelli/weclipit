7fef6540205dd222d8e6536ea51313af16d8cb81
Benjamin Chelli <benjamin@chelli.net>
2013-03-08 19:50:20 +0100

    Add Open graph for twitter

--- server/opengraph.js
+++ server/opengraph.js
@@ -12,6 +12,18 @@ if(Meteor.isServer){
     return false;
   }
 
+  function getTwitterVideoUrl(video){
+    switch(video.provider){
+      case 'youtube':
+        return 'https://www.youtube.com/embed/'+video.providerId;
+        break;
+      case 'vimeo':
+        return 'https://player.vimeo.com/video/'+video.providerId;
+        break;
+    }
+    return false;
+  }
+
   // Set Facebook APP ID
   Meteor.facebook.use({
     openGraphTags:function(req){
@@ -20,12 +32,30 @@ if(Meteor.isServer){
   });
 
   // Website
+  function isWebsite(req){
+    var url = __meteor_bootstrap__.require('url'); 
+    var parts = url.parse(req.url).pathname.split('/');
+    var absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url);
+    return (parts.length == 2 && parts[1] === '');
+  }
+  Meteor.twitter.use({
+    openGraphTags:function(req){
+      if(isWebsite(req)){
+        return [
+          {"property":'twitter:card',         "content":'summary'}
+        , {"property":'twitter:site',         "content":"@26plays"}
+        , {"property":'twitter:url',          "content":Meteor.absoluteUrl()}
+        , {"property":'twitter:title',        "content":'26 Plays'}
+        , {"property":'twitter:image',        "content":Meteor.absoluteUrl('img/logo.png')}
+        , {"property":'twitter:description',  "content":'Keep, play and share your videos with your friends'}
+        ];
+      }
+      return [];
+    }
+  });
   Meteor.facebook.use({
     openGraphTags:function(req){
-      var url = __meteor_bootstrap__.require('url'); 
-      var parts = url.parse(req.url).pathname.split('/');
-      var absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url);
-      if(parts.length == 2 && parts[1] === ''){
+      if(isWebsite(req)){
         return [
           {"property":'og:type',        "content":'website'}
         , {"property":'og:url',         "content":Meteor.absoluteUrl()}
@@ -34,21 +64,49 @@ if(Meteor.isServer){
         , {"property":'og:description', "content":'Keep, play and share your videos with your friends'}
         ];
       }
+      return [];
     }
   });
 
   // playlist
+  function isPlaylist(req){
+    var url = __meteor_bootstrap__.require('url'); 
+    var parts = url.parse(req.url).pathname.split('/');
+    if(parts.length == 3 && parts[1] === 'playlist'){
+      return parts[2];
+    }
+    return false;
+  }
+  Meteor.twitter.use({
+    openGraphTags:function(req){
+      var playlistId = isPlaylist(req);
+      if(playlistId){
+        var playlist = playlists.findOne({_id:playlistId})
+          , absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url)
+          ;
+        if(playlist){
+          return [
+            {"property":'twitter:type',        "content":'summary'}
+          , {"property":'twitter:site',        "content":"@26plays"}
+          , {"property":'twitter:url',         "content":absoluteUrl}
+          , {"property":'twitter:title',       "content":playlist.name}
+          , {"property":'twitter:image',       "content":Meteor.absoluteUrl('img/logo.png')}
+          , {"property":'twitter:description', "content":playlist.name+' by '+playlist.ownerData.profile.name}
+          ];
+        }
+      }
+      return [];
+    }
+  });
   Meteor.facebook.use({
     openGraphTags:function(req){
-      var url = __meteor_bootstrap__.require('url'); 
-      var parts = url.parse(req.url).pathname.split('/');
-      var absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url);
-      if(parts.length == 3 && parts[1] === 'playlist'){
-        var playlistId = parts[2]
-          , playlist = playlists.findOne({_id:playlistId})
+      var playlistId = isPlaylist(req);
+      var result = [];
+      if(playlistId){
+        var playlist = playlists.findOne({_id:playlistId})
           , videoList = videos.find({playlist:playlistId}).fetch()
+          , absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url)
           ;
-        var result = [];
         if(playlist){
           result.push({"property":'og:type',        "content":'twentysixplays:video_playlist'});
           result.push({"property":'og:url',         "content":absoluteUrl});
@@ -58,27 +116,51 @@ if(Meteor.isServer){
         }
         if(videoList){
           for(var i in videoList){
-            result.push({
-              "property":"og:video"
-            , "content":getFacebookVideoUrl(videoList[i])
+            result.push({"property":"og:video",     "content":getFacebookVideoUrl(videoList[i])
             });
           }
         }
-        return result;
       }
+      return result;
     }
   });
 
   // video
+  function isVideo(req){
+    var url = __meteor_bootstrap__.require('url'); 
+    var parts = url.parse(req.url).pathname.split('/');
+    var absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url);
+    if(parts.length == 5 && parts[1] === 'playlist' && parts[3] === 'video'){
+      return parts[4];
+    }
+    return false;
+  }
+  Meteor.twitter.use({
+    openGraphTags:function(req){
+      var videoId = isVideo(req);
+      if(videoId){
+        var video = videos.findOne({_id:videoId});
+        console.log(video);
+        if(video){
+          return [
+            {"property":'twitter:type',        "content":'player'}
+          , {"property":'twitter:site',        "content":"@26plays"}
+          , {"property":'twitter:url',         "content":absoluteUrl}
+          , {"property":'twitter:title',       "content":video.data.title}
+          , {"property":'twitter:image',       "content":video.data.thumbnail_url}
+          , {"property":'twitter:player',      "content":getTwitterVideoUrl(video)}
+          , {"property":'twitter:description', "content":video.data.description}
+          ];
+        }
+      }
+      return [];
+    }
+  });
   Meteor.facebook.use({
     openGraphTags:function(req){
-      var url = __meteor_bootstrap__.require('url'); 
-      var parts = url.parse(req.url).pathname.split('/');
-      var absoluteUrl = Meteor.absoluteUrl(req.url.substr(0,1)=='/' ? req.url.substr(1) : req.url);
-      if(parts.length == 5 && parts[1] === 'playlist' && parts[3] === 'video'){
-        var videoId = parts[4]
-          , video = videos.findOne({_id:videoId})
-          ;
+      var videoId = isVideo(req);
+      if(videoId){
+        var video = videos.findOne({_id:videoId});
         console.log(video);
         if(video){
           return [
@@ -90,8 +172,8 @@ if(Meteor.isServer){
           , {"property":'og:description', "content":video.data.description}
           ];
         }
-        return [];
       }
+      return [];
     }
   });
 }
