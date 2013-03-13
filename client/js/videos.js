6e6a501d86750a51ce06230b1c4eb0d439c86aeb
Benjamin Chelli <benjamin@chelli.net>
2013-03-13 17:09:05 +0100

    Add Youtube Author in search

--- client/js/videos.js
+++ client/js/videos.js
@@ -87,8 +87,9 @@ Template.videosTemplate.events({
                 + '  <td class="videos-preview"><div class="img-container"><img src="'+videosYoutube[i].media$group.media$thumbnail[0].url+'" /></div></td>'
                 + '  <td class="videos-description">'
                 + '    <div class="video-description-title">'+videosYoutube[i].title.$t+'</div>'
-                + '    <div class="video-description-origin">'+formatTime(videosYoutube[i].media$group.yt$duration.seconds)+'</div>'
+                + '    <div class="video-description-origin">by '+videosYoutube[i].author[0].name.$t+'</div>'
                 + '  </td>'
+                + '  <td class="videos-added-by">'+formatTime(videosYoutube[i].media$group.yt$duration.seconds)+'</td>'
                 + '</tr>'
                 ;
       }
