89a7729b450d0291ece403a965867bef05f15649
Benjamin Chelli <benjamin@chelli.net>
2013-03-19 10:07:50 +0100

    Try fix for vimeo player

--- client/js/player/vimeo.js
+++ client/js/player/vimeo.js
@@ -21,21 +21,21 @@ App.player.vimeo = function(playerId, events){
     
     // attach events
     // end of video
-    if(events && events.end) {
-      that.player.addEvent('finish', function(){
+    that.player.addEvent('finish', function(){
+      if(events && events.end) {
         events.end();
-      });
-    }
-    if(events && events.pause) {
-      that.player.addEvent('pause', function(){
+      }
+    });
+    that.player.addEvent('pause', function(){
+      if(events && events.pause) {
         events.pause();
-      });
-    }
-    if(events && events.play) {
-      that.player.addEvent('play', function(){
+      }
+    });
+    that.player.addEvent('play', function(){
+      if(events && events.play) {
         events.play();
-      });
-    }
+      }
+    });
     that.player.addEvent('loadProgress', function(obj){
       that.context.loadPercent = Math.floor(100*obj.percent)
       if(events && events.progress) events.progress(that.context);
