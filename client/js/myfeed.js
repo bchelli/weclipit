19af938d02346ca01b862b728f32e09f81083734
Benjamin Chelli <benjamin@chelli.net>
2013-03-28 14:13:14 +0100

    Fix some behaviours for the news feeds display/hide

--- client/js/myfeed.js
+++ client/js/myfeed.js
@@ -24,10 +24,15 @@ Template.myFeedTemplate.videos = function(){
   return Session.get('videos-list');
 };
 
-Template.myFeedTemplate.rendered = function(){
+Deps.autorun(function(){
   var u = Meteor.user();
-  // GET MY FRIENDS
+
+  Session.set('friends-list', []);
+  Session.set('videos-list', []);
+
+  // must be logged
   if(u && u.services) {
+    // GET MY FRIENDS
     var funct;
     if(u.services.facebook) funct = 'getFacebookFriends';
     if(u.services.twitter) funct = 'getTwitterFriends';
@@ -37,19 +42,38 @@ Template.myFeedTemplate.rendered = function(){
         if(!err) Session.set('friends-list', friends);
       });
     }
-  }
-  // GET LAST MOVIES ADDED
-  Meteor.call(
-    'getLastVideosAdded' 
-  , function(err, videos){
-      if(!err) {
-        for(var i=0,l=videos.length;i<l;i++){
-          videos[i].playlistData = playlists.findOne({_id:videos[i].playlist});
+    // GET LAST MOVIES ADDED
+    Meteor.call(
+      'getLastVideosAdded' 
+    , function(err, videos){
+        if(!err) {
+          for(var i=0,l=videos.length;i<l;i++){
+            videos[i].playlistData = playlists.findOne({_id:videos[i].playlist});
+          }
+          Session.set('videos-list', videos);
         }
-        Session.set('videos-list', videos);
       }
+    );
+  }
+});
+
+Deps.autorun(function(){
+  var lists = [
+          Session.get('videos-list')
+        , Session.get('friends-list')
+        ]
+    , display = false
+    ;
+  for(var i=0,l=lists.length;i<l;i++){
+    if(lists[i] && lists[i].length>0){
+      display = true;
     }
-  );
+  }
+  if(display) $('body').addClass('display-feeds');
+  else $('body').removeClass('display-feeds');
+});
+
+Template.myFeedTemplate.rendered = function(){
   // SET NICE SCROLL
   setNicescroll("#feedContent");
 };
