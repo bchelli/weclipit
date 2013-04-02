fb187ce903ebca20596398e53e5e343374d29200
Benjamin Chelli <benjamin@chelli.net>
2013-04-02 11:26:35 +0200

    Add few seconds formating for creation Date of videos

--- client/js/videos/list.js
+++ client/js/videos/list.js
@@ -24,6 +24,7 @@ Template.videosListTemplate.helpers({
 , formatTime: function(time){
     var delta = (new Date()).getTime() - time;
     if(_.isNaN(delta)) return '?';
+    if(delta <= 1000*60) return 'few seconds'
     if(delta >= 1000*60*60*24*365) return Math.floor(delta / (1000*60*60*24*365))+' year(s)';
     if(delta >= 1000*60*60*24*30) return Math.floor(delta / (1000*60*60*24*30))+' month(s)';
     if(delta >= 1000*60*60*24) return Math.floor(delta / (1000*60*60*24))+' day(s)';
