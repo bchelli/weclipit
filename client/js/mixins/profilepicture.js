50f8bcc66c2e85f2d995ef1abaaf1ca8dd56555e
Benjamin Chelli <benjamin@chelli.net>
2013-04-26 16:53:38 +0200

    Add default picture on error loading profile pictures

--- client/js/mixins/profilepicture.js
+++ client/js/mixins/profilepicture.js
@@ -1,5 +1,14 @@
 
-
 Template.profilePictureSharing.rendered = function(){
   $(this.find('img')).tooltip();
 };
+
+var ev = {
+  'error img':function(ev, tmpl){
+    $(ev.currentTarget).attr('src', '/img/logo-200px.png');
+  }
+};
+
+Template.profilePictureSharing.events(ev);
+Template.profilePicture.events(ev);
+Template.profilePictureLarge.events(ev);
